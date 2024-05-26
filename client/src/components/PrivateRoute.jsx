97b6adb2757eaf1b980852ef4318f925842d4299
vipt <vipt@vnpay.vn>
2024-05-26 18:00:21 +0700

    update header component and make profile private

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateRoute = () => {
+  const { currentUser } = useSelector((state) => state.user)
+  console.log(currentUser, 'currentUser')
+  // Outlet: if currentUser is not null, show children inside route
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
+}
+
+export default PrivateRoute
