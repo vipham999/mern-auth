4f5e1b123e5c83a19d6fa9bf0bf87dca7ada08da
vipt <vipt@vnpay.vn>
2024-06-04 21:37:12 +0700

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
-
+import path from 'path'
 dotenv.config()
 
 // Connect to MongoDB
@@ -17,9 +17,17 @@ mongoose
     console.log(err)
   })
 
+const __dirname = path.resolve()
+
 const app = express()
 // allow backend input JSON
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use(cookieParser())
 
